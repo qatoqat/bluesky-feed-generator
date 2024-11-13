be6dd2ce3e12318bee114ccd502abcaaea575759
Qatoqat <qatoqatt@gmail.com>
2024-11-14 06:55:57 +0800

    config

--- server/config.py
+++ server/config.py
@@ -1,16 +1,5 @@
 import os
 
-SERVICE_DID = os.environ.get('SERVICE_DID', None)
-HOSTNAME = os.environ.get('HOSTNAME', None)
-
-if HOSTNAME is None:
-    raise RuntimeError('You should set "HOSTNAME" environment variable first.')
-
-if SERVICE_DID is None:
-    SERVICE_DID = f'did:web:{HOSTNAME}'
-
-
-BOOKMARKS_URI = os.environ.get('BOOKMARKS_URI')
-if BOOKMARKS_URI is None:
-    raise RuntimeError('Publish your feed first (run publish_feed.py) to obtain Feed URI. '
-                       'Set this URI to "BOOKMARKS_URI" environment variable.')
+HOSTNAME = "biskuvibookmark.pythonanywhere.com"
+SERVICE_DID = f"did:plc:qvmvynssslo5yhstrnc2cwv6"
+BOOKMARKS_URI = f"at://{SERVICE_DID}/app.bsky.feed.generator/bookmarks"
